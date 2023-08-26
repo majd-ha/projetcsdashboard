377fda84773826369b9e8430a3e9c48210efe043
majd-ha <zienjm@gmail.com>
2023-08-26 13:05:23 +0300

    add cors

--- server.js
+++ server.js
@@ -17,7 +17,14 @@ const app = express();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://dashboard-6sko.onrender.com/",
+      "https://majd-ha.github.io/",
+    ],
+  })
+);
 app.use(express.static("public"));
 app.post("/login", loginUser);
 
