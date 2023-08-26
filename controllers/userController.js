da9b334519fd0452f0bc9398ed8eeb8f13d5e627
majd-ha <zienjm@gmail.com>
2023-08-26 12:04:22 +0300

    canceled a feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,32 +42,32 @@ module.exports = {
       res.status(400).json({ error: error.message });
     }
   },
+  //canceled
+  // signupUser: async (req, res) => {
+  //   console.log(req.body);
+  //   const { email, password } = req.body;
 
-  signupUser: async (req, res) => {
-    console.log(req.body);
-    const { email, password } = req.body;
+  //   try {
+  //     if (!email || !password) {
+  //       throw Error("all feilds must be filled");
+  //     }
+  //     if (!validator.isEmail(email)) {
+  //       throw Error("please enter a valid email");
+  //     }
+  //     const salt = await bcrypt.genSalt(10);
+  //     const hash = await bcrypt.hash(password, salt);
+  //     const user = await User.create({
+  //       email,
+  //       password: hash,
+  //     });
+  //     const token = createToken(user._id);
 
-    try {
-      if (!email || !password) {
-        throw Error("all feilds must be filled");
-      }
-      if (!validator.isEmail(email)) {
-        throw Error("please enter a valid email");
-      }
-      const salt = await bcrypt.genSalt(10);
-      const hash = await bcrypt.hash(password, salt);
-      const user = await User.create({
-        email,
-        password: hash,
-      });
-      const token = createToken(user._id);
-
-      res.status(201).json({
-        email: user.email,
-        token: token,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  },
+  //     res.status(201).json({
+  //       email: user.email,
+  //       token: token,
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // },
 };
